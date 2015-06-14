b4dd2cc871740df6eff927a536f7fa066a03061e
panamafrancis <anonwasere@gmail.com>
2015-06-14 11:55:38 +0200

    first working version, needs unit testing

---
+++ lsys.ml
@@ -0,0 +1,73 @@
+open Core.Std
+
+type ltree = Empty | 
+             Operand of operand | 
+             Branch of branch 
+             and operand = { value : string; next: ltree }
+             and branch = { left: ltree; right: ltree } 
+
+type lsystem = {
+            rules: (string*ltree) list; 
+            init: ltree
+}
+
+(*
+ * iterates up the tree, when it hits the root node it returns the 2nd tree
+ * which is then appended to the first 
+ * *)
+let rec append_tree l1 l2 = 
+    match l1 with
+    | Empty -> l2
+    | Branch {left=l; right=r} -> Branch{left=l; right=append_tree r l2}
+    | Operand {value=v; next=t} -> Operand{value=v; next= append_tree t l2}
+
+let rec insert_tree l1 l2 x = 
+    match l1 with
+    | Empty -> l2
+    | Branch {left=l; right=r} -> 
+            Branch{left=l; right=insert_tree r l2 x}
+    | Operand {value=v; next=t} when v = x -> 
+            Operand{value=v; next= insert_tree l2 t x}
+    | Operand {value=v; next=t} -> 
+            Operand{value=v; next= insert_tree t l2 x}
+
+let get_rule lrules r = 
+    try
+        List.Assoc.find_exn lrules r
+    with
+        (* If the operand is dead (like dead dna) then don't worry*)
+        Not_found -> Empty
+
+let rewrite lsys ~n =
+    let rec aux tree = 
+        match tree with
+        | Empty -> Empty
+        | Branch {left=l; right=r} ->
+                Branch{left=l; right=(aux r)}
+        | Operand{value=v; next=t} ->
+                let subtree = get_rule lsys.rules v 
+                in (
+                if subtree = Empty then
+                    Operand{value=v; next=(aux t)}
+                else
+                    append_tree subtree (aux t))
+
+    in let rec count lt m =
+        (* rewrite the ltree n times *)
+        if m > 0 then
+            count (aux lt) (m-1)
+        else (* return a new lsystem *)
+            {rules = lsys.rules; init = lt}
+
+    in count lsys.init n 
+
+let to_string lsys =
+    let rec aux tree = 
+        match tree with
+        | Empty -> ""
+        | Branch {left=l; right=r} ->
+                "[" ^ (aux l) ^ "]" ^ (aux r) 
+        | Operand {value=v; next=t} ->
+                v ^ (aux t)
+    in aux lsys
+
---
+++ lsys.mli
@@ -0,0 +1,49 @@
+open Core.Std
+
+type ltree = Empty | 
+             Operand of operand | 
+             Branch of branch 
+             and operand = { value : string; next: ltree }
+             and branch = { left: ltree; right: ltree } 
+(*
+ * An L-System rule or an iteration is stored in a tree
+ *   A
+ *     B
+ *      []
+ *     A  B
+ *      B  E
+ *       E
+ * Its string form would be AB[AB]B
+ *)
+
+type lsystem = {
+            rules: (string*ltree) list; 
+            init: ltree
+}
+(*
+ * An L-System consists of a set of re-write rules and the initial state of the
+ * system
+ *
+ *)
+
+(*val empty : lsystem*)
+
+val rewrite : lsystem -> n:int -> lsystem 
+(*
+ *  Generate the state of the L-System after n iterations
+ *)
+
+val append_tree : ltree -> ltree -> ltree
+(* 
+ * return a new tree of [l1::l2]
+ *)
+
+val insert_tree : ltree -> ltree -> bytes -> ltree
+(*
+ * insert the second tree after the node with value x
+ *)
+
+val to_string : ltree -> string
+
+
+
